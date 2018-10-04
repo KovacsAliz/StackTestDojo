0b65a4cb0e4547cc85d0d1b4ef7f5a031123545f
alice.kovacs <alice.kovacs@gmail.com>
2018-10-04 12:17:54 +0200

    Creation of stack, Push, Pop are working

---
+++ Stack.Tests/StackTests.cs
@@ -0,0 +1,87 @@
+﻿using NUnit.Framework;
+using StackDojoTest;
+using System;
+
+namespace StackDojo.Tests
+{
+    [TestFixture]
+    public class StackTests
+    {
+        private Stack stack;
+
+        [SetUp]
+        public void SetupTests()
+        {
+            //Arrange
+            stack = new Stack(5);
+        }
+
+        [Test]
+        public void Construct_WithGivenSize_CreateStack()
+        {
+            Assert.IsNotNull(stack);
+        }
+
+        [Test]
+        public void Push_GivenOneInt_LengthEquals1()
+        {
+            //Act
+            stack.Push(5);
+            //Assert
+            var expected = 1;
+            var actual = stack.Length;
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void Pop_Pushed5_Returns5()
+        {
+            //Arrange
+            stack.Push(5);
+            //Act
+            int actual = stack.Pop();
+            //Assert
+            int expected = 5;
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void Pop_EmptyStack_ThrowInValidOperationException()
+        {
+            //Act
+            var ex = Assert.Throws<InvalidOperationException>(() => stack.Pop());
+            //Assert
+            Assert.That(ex.Message, Is.EqualTo("It is an empty stack!"));
+        }
+
+        [Test]
+        public void Pop_GivenTwoElement_ReturnsElementsInBackward()
+        {
+            //Arrange
+            stack.Push(12);
+            stack.Push(24);
+            //Act
+            int last = stack.Pop();
+            int lastBefore = stack.Pop();
+            //Assert
+            Assert.AreEqual(24, last);
+            Assert.AreEqual(12, lastBefore);
+        }
+
+        [Test]
+        public void Push_GivenMoreElementThanSize_ThrowsStackOverflow()
+        {
+            //Arrange
+            stack.Push(12);
+            stack.Push(24);
+            stack.Push(36);
+            stack.Push(24);
+            stack.Push(24);
+            //Act
+            var ex = Assert.Throws<StackOverflowException>(() => stack.Push(54));
+            //Assert
+            Assert.AreEqual("Stack is full", ex.Message);
+        }
+
+    }
+}
---
+++ StackDojoTest/Stack.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace StackDojoTest
+{
+    public class Stack
+    {
+        private readonly int _size;
+        private readonly int[] _elements;
+
+        public int Length { get; private set; }
+
+        public Stack(int size)
+        {
+            _size = size;
+            _elements = new int[_size];
+        }
+
+        public void Push(int value)
+        {
+            if (Length+1 > _size)
+            {
+                throw new StackOverflowException("Stack is full");
+            }
+            _elements[Length] = value;
+            Length++;
+        }
+
+        public int Pop()
+        {
+            if (Length == 0)
+            {
+                throw new InvalidOperationException("It is an empty stack!");
+            }
+            
+            return _elements[--Length];
+        }
+    }
+}
